c33b5234dd352e54008a768593bea5bd8e18d7f8
SandeepArup <61278421+SandeepArup@users.noreply.github.com>
2024-09-18 14:17:47 +0530

    GSAGH-540 - strong name assembly (#21)
    * GSAGH-540 - strong name assembly
    
    * GSAGH-540 - increase build number

--- OasysUnits/AssemblyInfo.cs
+++ OasysUnits/AssemblyInfo.cs
@@ -5,5 +5,6 @@ using System;
 using System.Runtime.CompilerServices;
 
 [assembly: CLSCompliant(true)]
-[assembly: InternalsVisibleTo("OasysUnits.Tests")]
+[assembly: InternalsVisibleTo("OasysUnits.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100252fff973c5e1656f068966f12d1424bb2cbc813e9b14a62a6c7e71bd034d3043852c12fcc725fec2ed1f56bcdd57ecdb019b4e75db5df53da8fc2b0de176465c9ab8d87dbc45ef535992c7552f194d8dcedeef91f01563e81fa433bafd6cdad1fc410446ee85f437ec4d1e3fe0bafc9cf211c4b62f47b75fdffe47815b3e5e9")]
+
 
